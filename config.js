376da480176381f7505701ef6d53c362b6d6c8a9
Narendras <ahmadnarendra231@gmail.com>
2026-01-07 22:43:46 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 var config = {
     lines: [
-        "Dear stfu,",
+        "Dear Someone,",
         "i never knew how much you mean to me when we first started talking . until today,i still love you as much as i did since the very first day we met. ",
         " every since you came into my life, i felt like i was the happiest. i just want to let you know how much you mean to me and how much i appreciate you.",
         "you never failed to make me smile. things aren't getting better every since we fought, i just miss you so much. having random break downs, with you on my mind wondering if you're doing fine. im sorry i wasn't what you wanted,",
@@ -9,5 +9,5 @@ var config = {
        
     ],
     date:"2022-08-09T20:15:00",
-    names: ["☃︎【 Your Tanmay 】","‎ "]
+    names: ["☃︎【 Your fav person 】","‎ "]
 }
